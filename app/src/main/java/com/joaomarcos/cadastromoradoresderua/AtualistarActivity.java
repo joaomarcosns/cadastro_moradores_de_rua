1ca1b25f9077fb9237ef4453abce0f178dcb52cc
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-12-08 14:03:44 -0300

    Adicionando tela de atualizar

---
+++ app/src/main/java/com/joaomarcos/cadastromoradoresderua/AtualistarActivity.java
@@ -0,0 +1,14 @@
+package com.joaomarcos.cadastromoradoresderua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtualistarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atualistar);
+    }
+}
