9cba1d24559756857fac49f7db5c77c0d18f4512
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-12-08 11:39:49 -0300

    Initial commit

---
+++ app/src/main/java/com/joaomarcos/cadastromoradoresderua/MainActivity.java
@@ -0,0 +1,35 @@
+package com.joaomarcos.cadastromoradoresderua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button getStarted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iniciarComponente();
+        irCastro();
+    }
+
+    private void irCastro() {
+        getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void iniciarComponente() {
+        getStarted = findViewById(R.id.getStarted);
+    }
+}
