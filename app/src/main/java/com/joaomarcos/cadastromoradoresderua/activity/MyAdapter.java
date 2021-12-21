c4cd3d60ef5bfc5102bcf2118afc5f02796cf09b
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2021-12-21 10:35:24 -0300

    Metodo de Atualizar

--- app/src/main/java/com/joaomarcos/cadastromoradoresderua/activity/MyAdapter.java
+++ app/src/main/java/com/joaomarcos/cadastromoradoresderua/activity/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.joaomarcos.cadastromoradoresderua.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +41,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         holder.textNome.setText(morador.getNome());
         holder.textData.setText(morador.getDataNascimento());
         holder.textEndereco.setText(morador.getOrientacaoSexual());
+        holder.textOrientacao.setText(morador.getOrientacaoSexual());
+        holder.textRaca.setText(morador.getRaca());
 
     }
 
@@ -48,16 +51,29 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         return moradorDeRuaArrayList.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
-        private TextView textNome, textData, textEndereco;
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView textNome, textData, textEndereco, textOrientacao, textRaca;
         private ImageView btnDeletar, btnEditar;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textNome = itemView.findViewById(R.id.text_nome);
             textData = itemView.findViewById(R.id.text_data);
             textEndereco = itemView.findViewById(R.id.text_endereco);
-            btnDeletar = itemView.findViewById(R.id.btn_deletar);
-//            btnEditar = itemView.findViewById(R.id.btn_editar);
+            textOrientacao = itemView.findViewById(R.id.text_orientacao);
+            textRaca = itemView.findViewById(R.id.text_raca);
+//            btnDeletar = itemView.findViewById(R.id.btn_deletar);
+            btnEditar = itemView.findViewById(R.id.btn_editar);
+
+
+            btnEditar.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            MoradorDeRua moradorDeRua = moradorDeRuaArrayList.get(getAdapterPosition());
+            Intent intent = new Intent(context, UpdatedActivity.class);
+            intent.putExtra("moradorDeRua", moradorDeRua);
+            context.startActivity(intent);
         }
     }
 }
